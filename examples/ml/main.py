e3ba55f6a48a242bd55fbdc8b3e6fadba7f16917
Enrique Molina <enrique.molina@urv.cat>
2024-06-13 16:30:34 +0200

    declare dag ml jolteon

--- examples/ml/main.py
+++ examples/ml/main.py
@@ -12,14 +12,15 @@ if __name__ == "__main__":
 
     @flexorchestrator
     def main():
-        dag = DAG("video-analytics")
+        dag = DAG("machine-learning")
 
         stage0 = Stage(stage_id="stage0", func=dummy_func)
         stage1 = Stage(stage_id="stage1", func=dummy_func)
         stage2 = Stage(stage_id="stage2", func=dummy_func)
         stage3 = Stage(stage_id="stage3", func=dummy_func)
 
-        stage0 >> stage1 >> [stage2, stage3]
+        stage0 >> [stage1, stage2, stage3]
+        stage1 >> stage2
         stage2 >> stage3
 
         dag.add_stages([stage0, stage1, stage2, stage3])
