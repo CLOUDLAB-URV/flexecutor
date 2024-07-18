32d8897678a88b5f439c8e625cbc440700948590
Enrique Molina <enrique.molina@urv.cat>
2024-07-18 16:01:27 +0200

    adding chunker selector in titanic csv app

--- examples/titanic/main.py
+++ examples/titanic/main.py
@@ -1,15 +1,36 @@
-from dataplug.formats.generic.csv import partition_num_chunks, CSV
+from dataplug.formats.generic.csv import CSV
+from dataplug.formats.generic.csv import partition_num_chunks as preprocess_dynamic_csv
 from lithops import FunctionExecutor
 
 from examples.titanic.functions import train_model
 from flexecutor.storage.chunker import ChunkerTypeEnum, Chunker
+from flexecutor.storage.chunking_strategies import preprocess_static_csv
 from flexecutor.storage.storage import FlexInput, FlexOutput
 from flexecutor.utils.utils import flexorchestrator
 from flexecutor.workflow.dag import DAG
 from flexecutor.workflow.executor import DAGExecutor
 from flexecutor.workflow.stage import Stage
 
+CHUNKER_TYPE = "STATIC"
+
+
 if __name__ == "__main__":
+
+    if CHUNKER_TYPE == "STATIC":
+        chunker = Chunker(
+            prefix="titanic",
+            chunker_type=ChunkerTypeEnum.STATIC,
+            strategy=preprocess_static_csv,
+        )
+    elif CHUNKER_TYPE == "DYNAMIC":
+        chunker = Chunker(
+            prefix="titanic",
+            chunker_type=ChunkerTypeEnum.DYNAMIC,
+            strategy=preprocess_dynamic_csv,
+            cloud_object_format=CSV,
+        )
+    else:
+        raise ValueError(f"Chunker type {CHUNKER_TYPE} not supported")
 
     @flexorchestrator(bucket="test-bucket")
     def main():
@@ -18,22 +39,7 @@ if __name__ == "__main__":
         stage = Stage(
             stage_id="stage",
             func=train_model,
-            inputs=[
-                FlexInput(
-                    "titanic",
-                    # chunker=Chunker(
-                    #     prefix="titanic",
-                    #     chunker_type=ChunkerTypeEnum.STATIC,
-                    #     strategy=preprocess_static_csv,
-                    # ),
-                    chunker=Chunker(
-                        prefix="titanic",
-                        chunker_type=ChunkerTypeEnum.DYNAMIC,
-                        strategy=partition_num_chunks,
-                        cloud_object_format=CSV,
-                    ),
-                )
-            ],
+            inputs=[FlexInput("titanic", chunker=chunker)],
             outputs=[
                 FlexOutput(
                     prefix="titanic-accuracy",
