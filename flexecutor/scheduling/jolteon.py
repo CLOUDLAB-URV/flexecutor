c378d970595fdf2443c9a374a08f92425dc226df
Enrique Molina <enrique.molina@urv.cat>
2024-10-25 19:29:06 +0200

    removing pcpsolver class

--- flexecutor/scheduling/jolteon.py
+++ flexecutor/scheduling/jolteon.py
@@ -1,6 +1,4 @@
-import importlib
-import os
-from typing import Optional, Union
+import types
 
 import black
 import numpy as np
@@ -8,10 +6,20 @@ import numpy as np
 from modelling.perfmodel import PerfModelEnum
 from scheduling.orion import MyQueue
 from scheduling.scheduler import Scheduler
-from workflow.dag import DAG
 
 workers_accessor = slice(0, None, 2)
 cpu_accessor = slice(1, None, 2)
+
+
+def _get_sampling_size(num_stages, risk, confidence_error):
+    # Hoeffding's inequality
+    # Is incongruently but maintained for compatibility with Jolteon
+    # {0.5, 1, 1.5, 2, 3, 4} as the intra-function resource space, so the size is 8
+    # {4, 8, 16, 32} as the parallelism space, so the size is 4
+    search_space_size = (7 * 4) ** (num_stages // 2)  # num_X / 2 stages
+    return int(
+        np.ceil(1 / (2 * risk**2) * np.log(search_space_size / confidence_error))
+    )
 
 
 class Jolteon(Scheduler):
@@ -20,46 +28,28 @@ class Jolteon(Scheduler):
         super().__init__(dag, PerfModelEnum.MIXED)
         self.total_parallelism = total_parallelism
         self.cpu_per_worker = cpu_per_worker
-        self.cpu_search_space = [0.6, 1, 1.5, 2, 2.5, 3, 4]
-        self.workers_search_space = [1, 4, 8, 16, 32]
 
-    def schedule(self):
-        def get_sampling_size(num_stages, risk, confidence_error):
-            # Hoeffding's inequality
-            # Is incongruently but maintained for compatibility with Jolteon
-            # {0.5, 1, 1.5, 2, 3, 4} as the intra-function resource space, so the size is 8
-            # {4, 8, 16, 32} as the parallelism space, so the size is 4
-            search_space_size = (7 * 4) ** (num_stages // 2)  # num_X / 2 stages
-            return int(
-                np.ceil(
-                    1 / (2 * risk**2) * np.log(search_space_size / confidence_error)
-                )
-            )
+        self.num_X = 2 * len(dag.stages)
+        self.objective_fn = None
+        self.constraint_fn = None
 
-        sample_size = get_sampling_size(len(self._dag.stages), 0.05, 0.001)
-        print(f"Sample size: {sample_size}")
+        # FIXME: please parametrise as needed
 
-        # FIXME: allow parametrization
-        bound_type = "latency"
-        # noInspection PyUnresolvedReferences
-        samples = np.array(
-            [stage.perf_model.sample_offline(sample_size) for stage in self._dag.stages]
-        )
+        self.bound = 40
+        self.bound_type = "latency"  # "latency" or "cost"
+        self.probe_depth = 4
 
-        self._generate_func_code(
-            # FIXME: allow parametrization
-            self._dag.stages,
-            None,
-            bound_type,
-        )
+        self.obj_params = None  # 2-dim array --> shape: (num_stages, coeffs)
+        self.cons_params = None  # 3-dim array --> shape: (num_stages, coeffs, samples)
 
-        parameters = np.array(
-            [stage.perf_model.parameters() for stage in self._dag.stages]
-        )
+        # Used for probing
+        self.cpu_search_space = np.array([0.6, 1, 1.5, 2, 2.5, 3, 4])
+        self.workers_search_space = np.array([1, 4, 8, 16, 32])
 
-        my_bound = 40
-        x_init = [1, 3, 16, 3, 8, 3, 1, 3]
-        x_bound = [
+        entry_point = [1, 3, 16, 3, 8, 3, 1, 3]
+        self.x0 = entry_point if entry_point is not None else np.ones(self.num_X)
+
+        x_bounds = [
             (1, 2),
             (0.5, 4.1),
             (4, 32),
@@ -69,39 +59,60 @@ class Jolteon(Scheduler):
             (1, 2),
             (0.5, 4.1),
         ]
+        if isinstance(x_bounds, list):
+            self.x_bounds = x_bounds
+        else:
+            x_tuple = x_bounds if isinstance(x_bounds, tuple) else (0.5, None)
+            self.x_bounds = [x_tuple] * self.num_X
 
-        solver = PCPSolver(
-            dag=self._dag,
-            bound=my_bound,
-            bound_type=bound_type,
-            objective_params=parameters,
-            constraint_params=samples,
-            cpu_search_space=self.cpu_search_space,
-            workers_search_space=self.workers_search_space,
-            entry_point=x_init,
-            x_bounds=x_bound,
+        # User-defined risk level (epsilon) for constraint satisfaction (e.g., 0.01 or 0.05)
+        self.risk = 0.05
+
+        # Confidence error (delta) for the lower bound property of the ground-truth optimal
+        # objective value or the feasibility of the solution or both,
+        # depending on the relationship between epsilon and alpha, default to 0.01
+        self.confidence_error = 0.001
+
+        # Function tolerance for the optimization solver
+        self.fn_tol = self.risk * self.bound
+
+    def schedule(self):
+        # STEP 1: Use Hoeffding's inequality to determine the sampling size
+        sample_size = _get_sampling_size(
+            len(self._dag.stages), self.risk, self.confidence_error
         )
 
-        num_workers, num_cpu = solver.iter_solve()
+        # STEP 2: Extract the coefficients of the performance model
+        self.obj_params = np.array(
+            [stage.perf_model.parameters() for stage in self._dag.stages]
+        )
+
+        # STEP 3: Constraint Montecarlo sampling
+        self.cons_params = np.array(
+            [stage.perf_model.sample_offline(sample_size) for stage in self._dag.stages]
+        ).transpose((0, 2, 1))
+
+        # STEP 4: Generate the objective and constraint functions
+        self._generate_func_code(
+            # FIXME: allow parametrization
+            self._dag.stages,
+            None,
+        )
+
+        # STEP 5: Solve the optimization problem (gradient-based descent + probing)
+        num_workers, num_cpu = self._iter_solve()
         num_workers, num_cpu = self._round_config(num_workers, num_cpu)
-        num_workers, num_cpu = solver.probe(num_workers, num_cpu)
+        num_workers, num_cpu = self._probe(num_workers, num_cpu)
 
         print(f"Num CPU: {num_cpu}")
         print(f"Num Func: {num_workers}")
         print("Jolteon PCPSolver finished as expected!")
 
-    def _generate_func_code(
-        self,
-        critical_path,
-        secondary_path=None,
-        cons_mode="latency",
-    ):
+    def _generate_func_code(self, critical_path, secondary_path=None):
         assert isinstance(critical_path, list)
         assert secondary_path is None or isinstance(secondary_path, list)
-        assert cons_mode in ["latency", "cost"]
-        code_dir = os.path.dirname(os.path.abspath(__file__))
-        code_path = os.path.join(code_dir, self._dag.dag_id + "_func.py")
-        obj_mode = "cost" if cons_mode == "latency" else "latency"
+        obj_mode = "cost" if self.bound_type == "latency" else "latency"
+        cons_mode = self.bound_type
 
         code = """
 import numpy as np
@@ -159,8 +170,18 @@ def workers(stage):
                 code += _create_func_code(cons2_header, cons_mode, c_s, bound_value)
 
         code = black.format_str(code, mode=black.FileMode())
-        with open(code_path, "w") as f:
-            f.write(code)
+
+        # code_dir = os.path.dirname(os.path.abspath(__file__))
+        # code_path = os.path.join(code_dir, self._dag.dag_id + "_func.py")
+        # with open(code_path, "w") as f:
+        #     f.write(code)
+
+        module_name = f"flexecutor.scheduling.{self._dag.dag_id}_func"
+        module = types.ModuleType(module_name)
+        exec(code, module.__dict__)
+
+        self.objective_fn = getattr(module, "objective_func")
+        self.constraint_fn = getattr(module, "constraint_func")
 
     def _round_config(self, num_workers, num_cpu):
         rounded_num_workers = []
@@ -182,63 +203,7 @@ def workers(stage):
             rounded_num_cpu.append(c)
         return rounded_num_workers, rounded_num_cpu
 
-
-class PCPSolver:
-    def __init__(
-        self,
-        dag: DAG,
-        bound: float,
-        bound_type: str,
-        objective_params: np.ndarray,
-        constraint_params: np.ndarray,
-        cpu_search_space: list,
-        workers_search_space: list,
-        entry_point: Optional[list],
-        x_bounds: Union[list | tuple | None],
-    ):
-        self.num_X = 2 * len(dag.stages)
-
-        module = importlib.import_module(f"flexecutor.scheduling.{dag.dag_id}_func")
-        constraint_func = getattr(module, "constraint_func")
-        objective_func = getattr(module, "objective_func")
-
-        self.objective_fn = objective_func
-        self.constraint_fn = constraint_func
-        self.bound = bound
-        self.obj_params = (
-            objective_params  # 2-dim array --> shape: (num_stages, coeffs)
-        )
-        self.cons_params = np.transpose(
-            constraint_params, (0, 2, 1)
-        )  # 3-dim array --> shape: (num_stages, coeffs, samples)
-
-        # used for probing
-        self.cpu_search_space = np.array(cpu_search_space)
-        self.workers_search_space = np.array(workers_search_space)
-
-        self.x0 = entry_point if entry_point is not None else np.ones(self.num_X)
-        if isinstance(x_bounds, list):
-            self.x_bounds = x_bounds
-        else:
-            x_tuple = x_bounds if isinstance(x_bounds, tuple) else (0.5, None)
-            self.x_bounds = [x_tuple] * self.num_X
-
-        # FIXME: please rethink if we need to extract the values of next variables to user-defined parameters site
-
-        # User-defined risk level (epsilon) for constraint satisfaction (e.g., 0.01 or 0.05)
-        self.risk = 0.05
-        # Confidence error (delta) for the lower bound property of the ground-truth optimal
-        # objective value or the feasibility of the solution or both,
-        # depending on the relationship between epsilon and alpha, default to 0.01
-        self.confidence_error = 0.001
-
-        # Used for solving
-        self.ftol = self.risk * self.bound
-
-        self.bound_type = bound_type
-        self.probe_depth = 4
-
-    def iter_solve(self) -> tuple[list, list]:
+    def _iter_solve(self) -> tuple[list, list]:
         bound = self.bound
         while True:
             import scipy.optimize as scipy_opt
@@ -252,21 +217,21 @@ class PCPSolver:
                     "type": "ineq",
                     "fun": lambda x: -self.constraint_fn(x, self.cons_params, bound),
                 },
-                options={"ftol": self.ftol, "disp": False},
+                options={"ftol": self.fn_tol, "disp": False},
             )
 
             cons_val = np.array(
                 self.constraint_fn(minimize_result.x, self.cons_params, bound)
             )
-            ratio_not_satisfied = np.sum(cons_val > self.ftol) / len(cons_val)
+            ratio_not_satisfied = np.sum(cons_val > self.fn_tol) / len(cons_val)
             if ratio_not_satisfied < self.risk or minimize_result.success:
                 break
             print("bound:", bound, "ratio:", ratio_not_satisfied)
-            bound += self.ftol * 4
+            bound += self.fn_tol * 4
 
         return minimize_result.x[workers_accessor], minimize_result.x[cpu_accessor]
 
-    def probe(self, num_workers, num_cpu):
+    def _probe(self, num_workers, num_cpu):
         def get_x_by_xpos(array):
             result = np.zeros(self.num_X)
             result[workers_accessor] = self.workers_search_space[
